1a06cea8ba90da4d9f4c21371945bdca16601ff0
Felipe Rodriguez <felipersdot@gmail.com>
2024-08-17 12:16:39 -0500

    Remove ignored files from the repository

---
+++ build/static/js/63.deba54f2.chunk.js
@@ -0,0 +1,16 @@
+"use strict";(self.webpackChunkricocreations=self.webpackChunkricocreations||[]).push([[63],{4063:(e,a,c)=>{c.r(a),c.d(a,{default:()=>l});var s=c(8703),t=(c(5043),c(8887)),i=c(579);const d=t.Ay.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 16px;
+  padding: 16px;
+`,n=t.Ay.img`
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+  border-radius: 8px;
+`,r=()=>(0,i.jsxs)(d,{children:[(0,i.jsx)(n,{src:"http://images.squarespace-cdn.com/content/v1/5c72d15fc46f6d147ea478e5/1570575851469-YBAVZ06F9FZRYOSPMMG9/yasmin.jpg",alt:"Yasmin"}),(0,i.jsx)(n,{src:"http://images.squarespace-cdn.com/content/v1/5c72d15fc46f6d147ea478e5/1570575851005-O22SCLM2200NXLL0COL6/LOVELY+LAVENDER.jpg",alt:"Lovely Lavender"}),(0,i.jsx)(n,{src:"http://images.squarespace-cdn.com/content/v1/5c72d15fc46f6d147ea478e5/1570575849912-ZY71UJ5LWTNRBTZKZR8J/lolita.jpg",alt:"Lolita"}),(0,i.jsx)(n,{src:"http://images.squarespace-cdn.com/content/v1/5c72d15fc46f6d147ea478e5/1570575848050-XIMEV1ZFE18J155X2CN9/CITRUS+ISLAND.jpg",alt:"Citrus Island"})]}),p=()=>(0,i.jsx)(o,{children:(0,i.jsx)(r,{})}),o=t.Ay.div`
+    display: flex;
+    justify-content: center;
+    padding: 20px;
+  `,l=(0,s.PA)((()=>(0,i.jsx)(p,{})))}}]);
+//# sourceMappingURL=63.deba54f2.chunk.js.map
